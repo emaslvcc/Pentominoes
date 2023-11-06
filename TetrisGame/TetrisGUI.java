e9de01beca6dbe20740e68c41091e89489681d19
emaslvcc <emadasilvachagas@gmail.com>
2023-11-06 20:05:45 +0100

    Explanation comments

--- TetrisGame/TetrisGUI.java
+++ TetrisGame/TetrisGUI.java
@@ -1,7 +1,7 @@
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.io.FileNotFoundException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,75 +9,72 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-
-
-
+// This class extends "JPanel", which is a swing component used for creating panels in the GUI
 public class TetrisGUI extends JPanel {
 
-    
-
+    // This constructor initialiazes the Tetris GUI by setting up the layout manager and creating UI components
     public TetrisGUI() {
+
+        // This divides the panel into five regions: North, South, East, West, and Center
         this.setLayout(new BorderLayout());
         
-
-        // Menu Panel on the Left
+        // A new JPanel object named menuPanel is created and is set to contain 4 rows and 1 column
         JPanel menuPanel = new JPanel();
         menuPanel.setLayout(new GridLayout(4, 1));
 
-        // Main Content Panel
+        // An instance of the Game class is created. This is where the game is played
         Game mainContentPanel = new Game(5,15, 45);
-        this.addKeyListener(mainContentPanel);
+        this.addKeyListener(mainContentPanel); // This handles keyboard input for game control
 
-
-        // Start Button
+        // Start button implementation
         JButton startButton = this.createMenuButton("Start");
         startButton.addActionListener(e -> {
-            // Start game logic goes here
-            JOptionPane.showMessageDialog(this, " Not implemented yet", "start", JOptionPane.INFORMATION_MESSAGE);
-            
-            
 
+            // Start game logic goes here
+            JOptionPane.showMessageDialog(this, "Not implemented yet", "start", JOptionPane.INFORMATION_MESSAGE);
         });
         menuPanel.add(startButton);
 
-        // Reset Button
+        // Reset button implementation
         JButton resetButton = this.createMenuButton("Reset");
         resetButton.addActionListener(e -> {
+
             // Reset game logic goes here
-
             JOptionPane.showMessageDialog(this, "Not implemented yet", "reset", JOptionPane.INFORMATION_MESSAGE);
-
-
             mainContentPanel.reset();
         });
         menuPanel.add(resetButton);
 
-        // High Score Button
+        // High Score button implementation
         JButton highScoreButton = this.createMenuButton("High Scores");
         highScoreButton.addActionListener(e -> {
+
             // Display high scores logic goes here
-
-
             JOptionPane.showMessageDialog(this, "High Scores: Not implemented yet", "High Scores", JOptionPane.INFORMATION_MESSAGE);
         });
         menuPanel.add(highScoreButton);
 
-        // Instruction Button
+        // Instruction button implementation
         JButton instructionButton = this.createMenuButton("Instructions");
         instructionButton.addActionListener(e -> {
+
+            // Instructions logic goes here
             JOptionPane.showMessageDialog(this, this.getInstructions(), "Instructions", JOptionPane.INFORMATION_MESSAGE);
         });
         menuPanel.add(instructionButton);
 
+        // Adds the menu panel to the left of the GUI
         this.add(menuPanel, BorderLayout.WEST);
-;
-        
 
-
-        // Add the main content panel to the center
+        // Adds the main content panel to the center of the GUI
         this.add(mainContentPanel, BorderLayout.CENTER);
     }
 
+    /** 
+     * Creates a button
+     * @param text is the message to be displayed in the button
+     * @return the button
+     */
     private JButton createMenuButton(String text) {
         JButton button = new JButton(text);
         button.setPreferredSize(new Dimension(200, 0));
@@ -85,29 +82,31 @@ public class TetrisGUI extends JPanel {
         return button;
     }
 
+    /** 
+     * Displays the game's instructions
+     * @param void
+     * @return the instructions for the game to be played
+     */
     private String getInstructions() {
         return "Tetris Instructions:\n" +
                 "Use arrow keys to move and rotate the falling block.\n" +
                 "Have fun!";
     }
 
-    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Tetris");
-            frame.setSize(440, 710);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setResizable(false);
-            frame.setFocusable(false);
 
-            
+            JFrame frame = new JFrame("Tetris"); // A JFrame named frame is created
+            frame.setSize(440, 710); // Size is set to 440 width and 710 height
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // When the user closes the window, the application is terminated
+            frame.setResizable(false); // It cannot be resized by the user
+            frame.setFocusable(false); // The ability to focus the frame is disabled
         
-            TetrisGUI tetrisGUI = new TetrisGUI();
-            frame.getContentPane().add(tetrisGUI);
+            TetrisGUI tetrisGUI = new TetrisGUI(); // A TetrisGUI object named testrisGUI is created
+            frame.getContentPane().add(tetrisGUI); // The tetrisGUI instance is added to the frame
 
-            frame.setVisible(true);
+            frame.setVisible(true); // The game is visible to the user
         });
     }
-
 }
 

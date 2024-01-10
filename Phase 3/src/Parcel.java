c7327b2e20b1698dfba44921d7f54fd2e0117fbd
emaslvcc <emadasilvachagas@gmail.com>
2024-01-10 15:39:58 +0100

    Parcel file formatting

--- Phase 3/src/Parcel.java
+++ Phase 3/src/Parcel.java
@@ -1,42 +1,51 @@
-public class Parcel{
+public class Parcel {
     private int x;
     private int y;
     private int z;
     private int val;
     private int weight;
 
+    /**
+     * A constructor that creates a parcel object
+     * @param c a character correspondent to a parcel (A, B or C)
+     **/
     public Parcel(char c){
-        if(c == 'A'){
+        if (c == 'A') {
             this.x = 2;
             this.y = 2;
             this.z = 4;
-            this.weight = 2*2*4;
+            this.weight = 2 * 2 * 4;
             this.val = 3;
-        }
-        if(c == 'B'){
+        } 
+        else if (c == 'B') {
             this.x = 2;
             this.y = 3;
             this.z = 4;
-            this.weight = 2*3*4;
+            this.weight = 2 * 3 * 4;
             this.val = 4;
         }
-        if(c == 'C'){
+        else if (c == 'C') {
             this.x = 3;
             this.y = 3;
             this.z = 3;
-            this.weight = 3*3*3;
+            this.weight = 3 * 3 * 3;
             this.val = 5;
         }
-    
     }
 
+    /**
+     * Getter method that returns a parcel's weight
+     * @return parcel's weight
+     */
     public int getWeight(){
         return this.weight;
     }
 
+    /**
+     * Getter method that returns a parcel's value
+     * @return parcel's value
+     */
     public int getValue(){
         return this.val;
     }
-
-
 }

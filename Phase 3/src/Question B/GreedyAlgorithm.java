7e19594d567ac16b3adf3b17438b06f4434c5c01
Mika Hagenbeek <Mika.hagenbeek@Gmail.com>
2024-01-17 16:16:59 +0100

    organize

--- Phase 3/src/Question B/GreedyAlgorithm.java
+++ Phase 3/src/Question B/GreedyAlgorithm.java
@@ -1,8 +1,7 @@
-import SuperParcels.Parcel;
+
 
 public class GreedyAlgorithm {
 
-    // Create parcel objects
     Parcel parcelA = new Parcel('A');
     Parcel parcelB = new Parcel('B');
     Parcel parcelC = new Parcel('C');
@@ -10,35 +9,34 @@ public class GreedyAlgorithm {
     public final int WIDTH = 5;
     public final int LENGTH = 33;
     public final int HEIGHT = 8;
-    public int[][][] truck = new int[WIDTH][LENGTH][HEIGHT];
+    public int[][][] truck = new int[this.WIDTH][this.LENGTH][this.HEIGHT];
     public int score = 0;
-    public int added = 0;
 
     public void fillTruck() {
-        Parcel[] parcels = {parcelC, parcelB, parcelA};
+        Parcel[] parcels = {this.parcelC, this.parcelB, this.parcelA};
 
         for (Parcel parcel : parcels) {
             for (int rotation = 0; rotation < 6; rotation++) {
-                while(placementAttempt(parcel, rotation)) {
-                    added++;
+                parcel.resetRotation();
+                for (int i = 0; i < rotation; i++) {
+                    parcel.rotate();
                 }
+
+                boolean placementDone;
+                do {
+                    placementDone = this.tryPlaceParcel(parcel);
+                } while (placementDone);
             }
         }
     }
 
-    public boolean placementAttempt(Parcel parcel, int rotation) {
-        
-        while (rotation > 0) {
-            parcel.rotate();
-            rotation--;
-        }
-        
+    private boolean tryPlaceParcel(Parcel parcel) {
         int[][][] array = parcel.getParcelArray();
-        for (int x = 0; x < truck.length - array.length + 1; x++) {
-            for (int y = 0; y < truck[0].length - array[0].length; y++) {
-                for (int z = 0; z < truck[0][0].length - array[0][0].length; z++) {
-                    if (canAdd(parcel, x, y, z)) {
-                        addParcel(parcel, x, y, z);
+        for (int x = 0; x <= this.truck.length - array.length; x++) {
+            for (int y = 0; y <= this.truck[0].length - array[0].length; y++) {
+                for (int z = 0; z <= this.truck[0][0].length - array[0][0].length; z++) {
+                    if (this.canAdd(parcel, x, y, z)) {
+                        this.addParcel(parcel, x, y, z);
                         return true;
                     }
                 }
@@ -47,12 +45,12 @@ public class GreedyAlgorithm {
         return false;
     }
 
-    public boolean canAdd(Parcel parcel, int x, int y, int z) {
+    private boolean canAdd(Parcel parcel, int x, int y, int z) {
         int[][][] array = parcel.getParcelArray();
         for (int i = x; i < x + array.length; i++) {
             for (int j = y; j < y + array[0].length; j++) {
                 for (int k = z; k < z + array[0][0].length; k++) {
-                    if (truck[i][j][k] != 0) {
+                    if (this.truck[i][j][k] != 0) {
                         return false;
                     }
                 }
@@ -61,45 +59,44 @@ public class GreedyAlgorithm {
         return true;
     }
 
-    public int addParcel(Parcel parcel, int x, int y, int z) {
+    private void addParcel(Parcel parcel, int x, int y, int z) {
         int[][][] array = parcel.getParcelArray();
         for (int i = x; i < x + array.length; i++) {
             for (int j = y; j < y + array[0].length; j++) {
                 for (int k = z; k < z + array[0][0].length; k++) {
-                    truck[i][j][k] = parcel.getNum();
+                    this.truck[i][j][k] = parcel.getNum();
                 }
             }
         }
-        score += parcel.getValue();
-        return score;
+        this.score += parcel.getValue();
     }
 
+
     private boolean isTruckFilled() {
-        // Check if the entire truck is filled
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < LENGTH; j++) {
-                for (int k = 0; k < HEIGHT; k++) {
-                    if (truck[i][j][k] == 0) {
-                        return false; // Not completely filled
+        for (int i = 0; i < this.WIDTH; i++) {
+            for (int j = 0; j < this.LENGTH; j++) {
+                for (int k = 0; k < this.HEIGHT; k++) {
+                    if (this.truck[i][j][k] == 0) {
+                        return false;
                     }
                 }
             }
         }
-        return true; // Completely filled
+        return true;
     }
 
     public void printMatrix() {
         System.out.println("Final Truck Matrix:");
-        for (int i = 0; i < truck.length; i++) {
-            for (int j = 0; j < truck[0].length; j++) {
-                for (int k = 0; k < truck[0][0].length; k++) {
-                    System.out.print(truck[i][j][k] + " ");
+        for (int i = 0; i < this.truck.length; i++) {
+            for (int j = 0; j < this.truck[0].length; j++) {
+                for (int k = 0; k < this.truck[0][0].length; k++) {
+                    System.out.print(this.truck[i][j][k] + " ");
                 }
                 System.out.println();
             }
             System.out.println();
         }
-        System.out.println(score);
+        System.out.println("Total score: " + this.score);
     }
 
     public static void main(String[] args) {
---
+++ Phase 3/src/Question B/JavaFX.java
@@ -0,0 +1,233 @@
+import javafx.application.Application;
+import javafx.geometry.Point3D;
+import javafx.scene.AmbientLight;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application {
+
+    private static final int WIDTH = 1400;
+    private static final int HEIGHT = 800;
+
+    private Group group; // Move this line to the top of the class
+    private PerspectiveCamera camera;
+    private AmbientLight ambientLight;
+
+    private double lastX;
+    private double lastY;
+
+    private final int BLOCK_SIZE = 30;
+    private final int OFFSET = 0;
+    //private final int width = 75; // Move these declarations above their usage
+    //private final int height = 120;
+    //private final int depth = 415;
+    //private final int xCoord = (width * BLOCK_SIZE + (width * OFFSET)) / 2;
+    //private final int yCoord = (height * BLOCK_SIZE + (height * OFFSET)) / 2;
+    //private final int zCoord = (depth * BLOCK_SIZE + (depth * OFFSET)) / 2;
+
+    int[][][] exampleArray = {
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+        {{1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1},
+        {1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,1,1,1,1,2,2,1,1,1,1,1,1,2,1,1,1,1,1},
+        {1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1},
+        {1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1},},
+};
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        this.group = new Group();
+
+        // Creating a scene object colored in black
+        this.camera = new PerspectiveCamera();
+        this.ambientLight = new AmbientLight(Color.WHITE);
+        this.group.getChildren().add(this.ambientLight);
+
+        Scene scene = new Scene(this.group, WIDTH, HEIGHT, true, SceneAntialiasing.BALANCED);
+        scene.setFill(Color.WHITE);
+        scene.setCamera(this.camera);
+
+        // Set truck's position to the center of the GUI
+        this.group.translateXProperty().set(WIDTH / 2);
+        this.group.translateYProperty().set(HEIGHT / 2);
+
+        GreedyAlgorithm greedy = new GreedyAlgorithm();
+        greedy.fillTruck();
+        greedy.printMatrix();
+        this.exampleArray = greedy.truck;
+        this.drawParcel(this.exampleArray);
+        this.createContainerOutlines();
+        //drawParcel(Pentominoes.tPent);
+
+        // Setting title to the Stage
+        stage.setTitle("Truck Visualizer");
+        // Adding scene to the stage
+        stage.setScene(scene);
+        // Displaying the contents of the stage
+        stage.show();
+        // Enable mouse interaction for rotating the box
+        this.enableMouseInteraction(scene);
+    }
+
+    private void enableMouseInteraction(Scene scene) {
+        scene.setOnMousePressed(event -> {
+            this.lastX = event.getSceneX();
+            this.lastY = event.getSceneY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            double deltaX = event.getSceneX() - this.lastX;
+            double deltaY = event.getSceneY() - this.lastY;
+
+            double deltaXAngle = deltaX / scene.getWidth() * 360;
+            double deltaYAngle = deltaY / scene.getHeight() * 360;
+
+            Rotate rotateX = new Rotate(deltaYAngle, Rotate.X_AXIS);
+            Rotate rotateY = new Rotate(deltaXAngle, Rotate.Y_AXIS);
+
+            // Apply rotation transformations to the group (truck, parcels, and outlines)
+            this.group.getTransforms().addAll(rotateX, rotateY);
+
+            this.lastX = event.getSceneX();
+            this.lastY = event.getSceneY();
+        });
+    }
+
+    private PhongMaterial getColor(int value) {
+        final PhongMaterial material = new PhongMaterial();
+
+        switch(value) {
+            case 1:
+                material.setDiffuseColor(Color.RED);
+                break;
+            case 2:
+                material.setDiffuseColor(Color.BLUE);
+                break;
+            case 3:
+                material.setDiffuseColor(Color.PURPLE);
+                break;
+            default:
+                return null;
+        }
+        return material;
+    }
+
+    private void drawParcel(int[][][] array) {
+
+        this.group.getChildren().clear();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    if (array[i][j][k] == 0) {
+                        continue;
+                    }
+
+                    PhongMaterial color = this.getColor(array[i][j][k]);
+                    Box box = new Box(30, 30, 30);
+                    box.setMaterial(color);
+                    box.setTranslateX((k * this.BLOCK_SIZE) + (k * this.OFFSET));
+                    box.setTranslateY((j * this.BLOCK_SIZE) + (j * this.OFFSET));
+                    box.setTranslateZ((i * this.BLOCK_SIZE) + (i * this.OFFSET));
+                    this.group.getChildren().add(box);
+                }
+            }
+        }
+    }
+
+    public void createContainerOutlines() {
+        int boxWidth = 120 * 2;
+        int boxHeight = 495 * 2;
+        int boxDepth = 75 * 2;
+        int offset = -(this.BLOCK_SIZE/2);
+        Point3D p1 = new Point3D(offset, offset, offset);
+        Point3D p2 = new Point3D(boxWidth + offset, offset, offset);
+        Point3D p3 = new Point3D(offset, boxHeight + offset, offset);
+        Point3D p4 = new Point3D(boxWidth + offset, boxHeight + offset, offset);
+        Point3D p5 = new Point3D(offset, offset, boxDepth + offset);
+        Point3D p6 = new Point3D(boxWidth + offset, offset, boxDepth + offset);
+        Point3D p7 = new Point3D(offset, boxHeight + offset, boxDepth + offset);
+        Point3D p8 = new Point3D(boxWidth + offset, boxHeight + offset, boxDepth + offset);
+
+        this.createLine(p1, p2);
+        this.createLine(p1, p3);
+        this.createLine(p1, p5);
+        this.createLine(p2, p6);
+        this.createLine(p2, p4);
+        this.createLine(p3, p4);
+        this.createLine(p3, p7);
+        this.createLine(p4, p8);
+        this.createLine(p5, p6);
+        this.createLine(p5, p7);
+        this.createLine(p6, p8);
+        this.createLine(p7, p8);
+    }
+
+    public void createLine(Point3D origin, Point3D target) { // Math taken from math stackexchange question #42124225
+
+        double width = 2.0;
+
+        Point3D yPoint = new Point3D(0, 1, 0);
+        Point3D diff = target.subtract(origin);
+        double height = diff.magnitude();
+
+        Point3D mid = target.midpoint(origin);
+        Translate moveToOriginCenter = new Translate(mid.getX(), mid.getY(), mid.getZ());
+
+        Point3D rAxis = diff.crossProduct(yPoint);
+        double angle = Math.acos(diff.normalize().dotProduct(yPoint));
+        Rotate rotate = new Rotate(-Math.toDegrees(angle), rAxis);
+
+        Cylinder line = new Cylinder(width, height);
+
+        line.getTransforms().addAll(moveToOriginCenter, rotate);
+        this.group.getChildren().add(line);
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}

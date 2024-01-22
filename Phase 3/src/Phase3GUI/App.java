023933206411ed4df23af95e8b6a2d4232938887
emaslvcc <emadasilvachagas@gmail.com>
2024-01-22 17:56:29 +0100

    Code cleaning and formatting

--- Phase 3/src/Phase3GUI/App.java
+++ Phase 3/src/Phase3GUI/App.java
@@ -1,6 +1,15 @@
 import javafx.application.Application;
 
+/**
+ * The main class responsible for launching the JavaFX application.
+ */
 public class App {
+
+    /**
+     * The main entry point for the Java application.
+     *
+     * @param args The command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
         Application.launch(JavaFX1.class, args);
     }
